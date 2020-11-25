3ce6eef17ab04c596f9dd3c43048af94136c5037
gtfisher <grant@chaosiq.io>
2020-11-25 10:58:51 +0000

    fixed index test
    Signed-off-by: gtfisher <grant@chaosiq.io>

--- test/index/index.spec.js
+++ test/index/index.spec.js
@@ -4,14 +4,14 @@ import index from '../../pages/index'
 describe('index', () => {
   test('mounts properly', () => {
     const wrapper = shallowMount(index, {
-      stubs: ['Logo'],
+      stubs: ['Logo', 'NuxtLink'],
     })
     expect(wrapper.vm).toBeTruthy()
   })
 
   test('renders properly', () => {
     const wrapper = shallowMount(index, {
-      stubs: ['Logo'],
+      stubs: ['Logo', 'NuxtLink'],
     })
     expect(wrapper.html()).toMatchSnapshot()
   })
